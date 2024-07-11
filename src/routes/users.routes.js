57a5b042a6ca9f31d083a4dc17b0a42e27611dd9
Fhupsel <fmhupsel@outlook.com>
2024-07-11 08:06:58 -0300

    middleware

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,10 +4,23 @@ const UsersController = require("../controllers/UsersController")
 
 const userRouters = Router()
 
+function myMyddleware(request, response, next){
+  console.log("Chegou no Middleware")
+  // const senha = request.body.password
+  // if(senha.length >= 8){
+  //   console.log("Senha aprovada")
+  //   next()
+  // }
+  if(!request.body.isAdmin){
+    return response.json({message: "user unauthorized"})
+  } 
+  next()
+}
+
 const usersController = new UsersController()
 
 ///POST => Body params
-userRouters.post("/", usersController.create) 
+userRouters.post("/", myMyddleware, usersController.create) 
 
 ///Exportando o arquivo
 module.exports = userRouters

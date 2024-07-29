4e6eaf4f76d54943dd325f12a6e35c6e9bac84d7
Fhupsel <fmhupsel@outlook.com>
2024-07-29 18:02:02 -0300

    put

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -35,8 +35,8 @@ class UsersController {
       throw new AppError("Este e-mail já está em uso!")
     }
 
-    user.name = name
-    user.email = email
+    user.name = name ?? user.name ///Caso name esteja vazio
+    user.email = email ?? user.email
 
     if(password && !old_password){
       throw new AppError("Você precisa informar a senha antiga para definir a nova senha")
@@ -56,9 +56,9 @@ class UsersController {
       name = ?,
       email = ?,
       password = ?,
-      updated_at = ?
+      updated_at = DATETIME('now')
       WHERE id = ?`, 
-      [user.name, user.email, user.password, new Date(), id]
+      [user.name, user.email, user.password, id]
     )
 
     return response.status(200).json()

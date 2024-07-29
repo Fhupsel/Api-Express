5b4023e8a5156d69107bbc86b4e26fb1177595f0
Fhupsel <fmhupsel@outlook.com>
2024-07-29 07:49:00 -0300

    updt

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,17 +1,68 @@
 ///No máximo 5 métodos (index, show, create, update, delete)
+const {hash, compare} = require("bcryptjs")
 
 const AppError = require('../utils/AppError')
 
+const sqliteConnection = require("../database/sqlite")
+
 class UsersController {
-  create(request, response) {
+  async create(request, response) {
     const { name, email, password } = request.body
 
-    if (!name) {
-      throw new AppError('Nome é obrigatório')
+    const database = await sqliteConnection()
+    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])  /// (?) => email
+    if(checkUserExists){
+      throw new AppError("Este e-mail já está em uso")
+    }
+    const hashedPassword = await hash(password, 8)
+    await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, hashedPassword])
+
+    return response.status(201).json()
+  }
+
+  async update(request, response){
+    const {name, email, password, old_password} = request.body
+    const {id} = request.params
+
+    const database = await sqliteConnection()
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id])
+    if(!user) {
+      throw new AppError("Usuário não existe")
     }
 
-    response.status(201).json({ name, email, password })
+    const userWithUpdateEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]) 
+    if(userWithUpdateEmail && userWithUpdateEmail.id !== user.id){
+      throw new AppError("Este e-mail já está em uso!")
+    }
+
+    user.name = name
+    user.email = email
+
+    if(password && !old_password){
+      throw new AppError("Você precisa informar a senha antiga para definir a nova senha")
+    }
+
+    if(password && old_password){
+      const checkOldPassword = await compare(old_password, user.password)
+
+      if(!checkOldPassword){
+        throw new AppError("A senha antiga não confere")
+      }
+      user.password = await hash(password, 8)
+    }
+
+    await database.run(`
+      UPDATE users SET
+      name = ?,
+      email = ?,
+      password = ?,
+      updated_at = ?
+      WHERE id = ?`, 
+      [user.name, user.email, user.password, new Date(), id]
+    )
+
+    return response.status(200).json()
   }
-}
+} 
 
 module.exports = UsersController
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 require('express-async-errors')
-const database = require("./database/sqlite")
+const migrationsRun = require("./database/sqlite/migrations")
 
 const AppError = require('./utils/AppError')
 
@@ -12,7 +12,7 @@ app.use(express.json())
 
 app.use(routes)
 
-database()
+migrationsRun()
 
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {

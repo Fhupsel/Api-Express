6fbf1c2ac885fc7d95cde1612a08f21718df152c
Fhupsel <fmhupsel@outlook.com>
2024-07-30 20:59:13 -0300

    orm

---
+++ knexfile.js
@@ -0,0 +1,18 @@
+const path = require('path')
+
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: path.resolve(__dirname, 'src', 'database', 'database.db')
+    },
+    pool: {
+      afterCreate: (conn, cb) => conn.run("PRAGMA foreing_keys = ON", cb)
+      ///Usar on cascade
+    },
+    migrations: {
+      directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
+    },
+    useNullAsDefault: true
+  }
+};
